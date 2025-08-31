678448397137a09d4329cecac4f33fff28f2b38c
ahmedElsayed-bob <ahmedHashem.elsayed@ai71.ai>
2025-08-31 15:06:09 +0400

    remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import FarmHeatmap from "@/screens/farms/farm-heatmap";
-
 export default function Home() {
-  return (
-    <div className="h-full">
-      <FarmHeatmap />
-    </div>
-  );
+  return <div className="h-full">{/* <FarmHeatmap /> */}</div>;
 }
